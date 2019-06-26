e9b1bcf68b8dbf3868a233eaebf97486332e6c1b
Ubuntu <ubuntu@ip-172-31-16-191.us-east-2.compute.internal>
2019-06-26 05:40:30 +0000

    first

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+const viewdir = path.join(__dirname + '/views');
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(viewdir +'/index.html'));
+});
+
+app.use('/', router);
+
+app.listen(8080);
+
+console.log('Running at port 8080...');
